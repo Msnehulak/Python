208c12ce0f41e922c682058067b5783f45bf34c4
Msnehulak <165189636+Msnehulak@users.noreply.github.com>
2025-08-08 19:11:28 +0200

    update

--- Projekty/Tools/CreatSearch.py
+++ Projekty/Tools/CreatSearch.py
@@ -2,6 +2,28 @@ import requests
 
 out = []
 
+# Movies
 Movies = requests.get('https://raw.githubusercontent.com/Msnehulak/Python/refs/heads/main/Data/Dictionary/Movies.txt')
 Movies = Movies.text.splitlines()
-print(Movies)
+toadd = ['', 'IMBb', 'cast', 'awards', 'trailer', 'reviews', 'director', 'soundtrack', 'box office', 'release date', 'free to watch', 'where to watch', 
+         'free to download', 'budget', 'runtime', 'language', 'country', 'rating', 'genre', 'production companies', 'distributor', 'writer', 
+         'editor', 'cinematography', 'music composer']
+
+for i in range(len(Movies)):
+    for x in range(len(toadd)):
+        out.append(Movies[i] + ' ' + toadd[x])
+
+# Series
+Series = requests.get('https://raw.githubusercontent.com/Msnehulak/Python/refs/heads/main/Data/Dictionary/series.txt')
+Series = Series.text.splitlines()
+toadd = ['', 'IMBb', 'cast', 'awards', 'trailer', 'reviews', 'director', 'soundtrack', 'release date', 'free to watch', 'where to watch', 'free to download', 
+         'seasons', 'episodes', 'network', 'status', 'premiere date', 'finale date']
+
+for i in range(len(Series)):
+    for x in range(len(toadd)):
+        out.append(Series[i] + ' ' + toadd[x])
+        
+print(len(out))
+
+
+
